344724826f5af8865f004cbb9a9327d3c06ca259
trekswithnick <nicholasbrowningdev@outlook.com>
2025-04-05 18:52:21 +1000

    created dummy routes

--- server.js
+++ server.js
@@ -5,13 +5,268 @@ require("./config/dbConnect")
 
 const app = express();
 
-// middlewards
+// middleware
 
 
 // routes
 
+// users route
+// POST/api/v1/users/register
+app.post('/api/v1/users/register', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'user registered'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
 
-// error handler middleward
+// POST/api/v1/users/login
+app.post('/api/v1/users/login', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'user login'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET/api/v1/users/profile/:id
+app.get('/api/v1/users/profile/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'user profile found'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET/api/v1/users
+app.get('/api/v1/users', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'profiles found'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// DELETE/api/v1/users/:id
+app.delete('/api/v1/users/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'user profile deleted'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// PUT/api/v1/users/:id
+app.put('/api/v1/users/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'user profile updated'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// posts route
+// POST/api/v1/posts
+app.post('/api/v1/posts', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'post created'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET/api/v1/posts/:id
+app.get('/api/v1/posts/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'post found'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET/api/v1/posts
+app.get('/api/v1/posts', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'posts found'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// DELETE/api/v1/posts/:id
+app.delete('/api/v1/posts/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'post deleted'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// PUT/api/v1/posts/:id
+app.put('/api/v1/posts/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'post updated'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// comments route
+// POST/api/v1/comments
+app.post('/api/v1/comments', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'comment created'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET/api/v1/comments
+app.get('/api/v1/comments', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'comments found'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET/api/v1/comments/:id
+app.get('/api/v1/comments/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'comment found'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// DELETE/api/v1/comments/:id
+app.delete('/api/v1/comments/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'comment deleted'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// PUT/api/v1/comments/:id
+app.put('/api/v1/comments/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'comment updated'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// categories route
+// POST/api/v1/comments
+app.post('/api/v1/categories', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'category created'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET/api/v1/comments
+app.get('/api/v1/categories', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'categories found'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET/api/v1/comments/:id
+app.get('/api/v1/categories/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'category found'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// DELETE/api/v1/comments/:id
+app.delete('/api/v1/categories/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'category deleted'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// PUT/api/v1/comments/:id
+app.put('/api/v1/categories/:id', async() => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'category updated'
+        });
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// error handler middleware
 
 
 // listening to the server
